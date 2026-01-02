70a29c47173e58f4ab2511c52bece757a1719e28
isaonaranjo <mionaranjo@gmail.com>
2026-01-02 11:09:42 -0600

    new image

--- home.py
+++ home.py
@@ -24,7 +24,7 @@ def home_page():
 
 
     # ----- Profile image file -----
-    profile_image_file_path = "isa.jfif"       # TODO: Upload your profile image to the same folder as this script and update this if it has a different name
+    profile_image_file_path = "isa.png"       # TODO: Upload your profile image to the same folder as this script and update this if it has a different name
 
     with open(profile_image_file_path, "rb") as img_file:
         img = "data:image/png;base64," + base64.b64encode(img_file.read()).decode()
